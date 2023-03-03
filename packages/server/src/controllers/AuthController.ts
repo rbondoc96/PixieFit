115ff29bc968b2331506d12954a5dbc032c42df4
Rodrigo Bondoc <rbondoc96@gmail.com>
2023-03-02 18:06:25 -0800

    SERVER update: cleanup AuthController and error middleware

--- packages/server/src/controllers/AuthController.ts
+++ packages/server/src/controllers/AuthController.ts
@@ -1,14 +1,5 @@
-import bcrypt from 'bcrypt';
 import type {NextFunction, Response, Request} from 'express';
-import {Error as MongooseError} from 'mongoose';
 
-import config from '@/core/config';
-import UserLoginData from '@/data/user/UserLoginData';
-import UserAuthenticationError from '@/errors/user/UserAuthenticationError';
-import UserRegistrationError from '@/errors/user/UserRegistrationError';
-import {createJwtToken} from '@/lib/auth/tokens';
-import Logger from '@/lib/Logger';
-import UnexpectedServerError from '@/errors/UnexpectedServerError';
 import User from '@/models/User';
 
 interface LoginUserRequest extends Request {
@@ -31,88 +22,76 @@ interface RegisterUserRequest extends Request {
     };
 }
 
-const login = async (req: LoginUserRequest, res: Response) => {
-    const {email, password} = req.body;
+export default class AuthController {
+    public static async login(
+        req: LoginUserRequest,
+        res: Response,
+        next: NextFunction,
+    ): Promise<void> {
+        const {email, password} = req.body;
 
-    try {
-        const user = await User.findOne({email});
-
-        if (user === null) {
-            res.sendApiError(new UserAuthenticationError());
-            return;
-        }
-
-        if (!(await bcrypt.compare(password, user.password))) {
-            res.sendApiError(new UserAuthenticationError());
-            return;
-        }
-
-        const token = createJwtToken(user._id);
-        res.cookie('jwt', token, {
-            httpOnly: true,
-            maxAge: config('auth.jwt.maxAge') * 1000,
-        });
-        res.sendApiData(new UserLoginData(user));
-    } catch (error: unknown) {
-        Logger.error(error);
-
-        if (error instanceof Error) {
-            res.sendApiError(new UnexpectedServerError(error));
+        try {
+            await req.login(email, password);
+            res.sendApiResponse({
+                message: 'Login successful.',
+            });
+        } catch (error: unknown) {
+            next(error);
         }
     }
-};
 
-// TODO: Implement jwt_blacklist table
-const logout = (req: Request, res: Response, next: NextFunction) => {
-    res.cookie('jwt', '');
-    res.status(200).json({
-        message: 'Logout successful.',
-    });
-};
+    // TODO: Implement jwt_blacklist table
+    public static async logout(
+        _req: Request,
+        res: Response,
+        _next: NextFunction,
+    ): Promise<void> {
+        res.cookie('jwt', '');
+        res.status(200).json({
+            message: 'Logout successful.',
+        });
+    }
 
-const register = async (
-    req: RegisterUserRequest,
-    res: Response,
-): Promise<void> => {
-    const {
-        birthday,
-        email,
-        first_name,
-        goal,
-        height,
-        last_name,
-        password,
-        sex,
-    } = req.body;
-
-    try {
-        const user = await User.create({
+    public static async register(
+        req: RegisterUserRequest,
+        res: Response,
+        next: NextFunction,
+    ): Promise<void> {
+        const {
             birthday,
             email,
             first_name,
             goal,
-            height_cm: height,
+            height,
             last_name,
             password,
             sex,
-        });
+        } = req.body;
 
-        const token = createJwtToken(user._id);
-        res.cookie('jwt', token, {
-            httpOnly: true,
-            maxAge: config('auth.jwt.maxAge') * 1000,
-        });
-        res.sendApiData(new UserLoginData(user));
-    } catch (error: unknown) {
-        if (error instanceof MongooseError.ValidationError) {
-            res.sendApiError(new UserRegistrationError(error));
-            return;
-        }
+        try {
+            const user = await User.create({
+                birthday,
+                email,
+                first_name,
+                goal,
+                height_cm: height,
+                last_name,
+                password,
+                sex,
+            });
 
-        if (error instanceof Error) {
-            res.sendApiError(new UnexpectedServerError(error));
+            req.issueJwt(user);
+            res.sendApiResponse(
+                {
+                    message: 'Login successful.',
+                },
+                {
+                    status: 200,
+                    success: true,
+                },
+            );
+        } catch (error: unknown) {
+            next(error);
         }
     }
-};
-
-export default {login, logout, register};
+}
--- packages/server/src/middleware/errors/catch.ts
+++ packages/server/src/middleware/errors/catch.ts
@@ -1,10 +1,32 @@
 import {NextFunction, Request, Response} from 'express';
+import {Error as MongooseError} from 'mongoose';
+
+import UserAuthenticationError from '@/errors/user/UserAuthenticationError';
+import UserRegistrationError from '@/errors/user/UserRegistrationError';
+import Logger from '@/lib/Logger';
 
 export default (
     error: Error,
-    req: Request,
+    _req: Request,
     res: Response,
-    next: NextFunction,
+    _next: NextFunction,
 ) => {
-    res.status(500).json({error});
+    Logger.error(error);
+    switch (true) {
+        case error instanceof UserAuthenticationError: {
+            res.sendApiError(error as UserAuthenticationError);
+            break;
+        }
+        case error instanceof MongooseError.ValidationError: {
+            res.sendApiError(
+                new UserRegistrationError(
+                    error as MongooseError.ValidationError,
+                ),
+            );
+            break;
+        }
+        default: {
+            res.status(500).json({error});
+        }
+    }
 };
