637d9f401eddb74d8b08af0b8894986c3fb49499
Rodrigo Bondoc <rbondoc96@gmail.com>
2023-08-29 05:58:40 -0700

    refactor(api): use async-controller factory for AuthController

--- packages/api/src/http/controllers/AuthController.ts
+++ packages/api/src/http/controllers/AuthController.ts
@@ -1,5 +1,4 @@
-import type {NextFunction, Response} from 'express';
-import asyncHandler from 'express-async-handler';
+import Controller from '@pxeeio/express-async-controller';
 import {DateTime} from 'luxon';
 import postgres from 'postgres';
 
@@ -8,105 +7,128 @@ import ModelNotFoundException from '@/exceptions/ModelNotFoundException';
 import QueryException from '@/exceptions/QueryException';
 import UserLoginException from '@/exceptions/UserLoginException';
 import ValidationException from '@/exceptions/ValidationException';
-import type LoginRequest from '@/http/requests/LoginRequest';
-import type RegisterRequest from '@/http/requests/RegisterRequest';
 import * as passwords from '@/lib/passwords';
 import User from '@/models/User';
 
-const index = asyncHandler(async (request, response: Response) => {
-    if (request.user === undefined) {
-        throw new AuthenticationException();
-    }
+type LoginRequest = {
+    body: {
+        email: string;
+        password: string;
+    };
+};
 
-    response.ok().jsonResponse({
-        data: request.user.toJSONResource(),
-    });
-});
+type RegisterRequest = {
+    body: {
+        birthday: string;
+        email: string;
+        first_name: string;
+        last_name: string;
+        password: string;
+        password_confirm: string;
+    };
+};
 
-const login = asyncHandler(async (request: LoginRequest, response: Response) => {
-    const {email, password} = request.body;
-
-    if (email === undefined) {
-        throw new ValidationException('email', ['An email is required']);
-    }
-
-    if (password === undefined) {
-        throw new ValidationException('password', ['A password is required']);
-    }
-
-    try {
-        const user = await User.findByEmail(email);
-
-        if ((await user?.verifyPassword(password)) === true) {
-            request.session.user = {
-                id: user.id,
-            };
-            response.ok().jsonResponse({
-                data: user.toJSONResource(),
-            });
-            return;
-        }
-    } catch (error) {
-        if (error instanceof ModelNotFoundException) {
-            throw new UserLoginException();
-        }
-
-        throw error;
-    }
-
-    throw new UserLoginException();
-});
-
-const logout = asyncHandler(async (request, response: Response, next: NextFunction) => {
-    request.session.destroy(function (error) {
-        // request.session is now undefined here
-
-        if (error) {
-            next(error);
+export default Controller<{
+    index: undefined;
+    login: LoginRequest;
+    logout: undefined;
+    register: RegisterRequest;
+}>({
+    index: async (request, response) => {
+        if (request.user === undefined) {
+            throw new AuthenticationException();
         }
 
         response.ok().jsonResponse({
-            data: null,
-            message: 'Successfully logged out.',
+            data: request.user.toJSONResource(),
         });
-    });
-});
+    },
+    login: async (request, response) => {
+        const {email, password} = request.body;
 
-const register = asyncHandler(async (request: RegisterRequest, response: Response) => {
-    const {birthday, email, first_name, last_name, password, password_confirm} = request.body;
-
-    if (password !== password_confirm) {
-        throw new ValidationException('password', ['Passwords do not match.']);
-    }
-
-    await passwords.validatePasswordPattern(password);
-
-    try {
-        const user = await User.create({
-            birthday: DateTime.fromISO(birthday, {setZone: false}).toJSDate(),
-            email,
-            first_name,
-            last_name,
-            password,
-        });
-
-        request.session.user = user;
-
-        response.created().jsonResponse({
-            data: user.toJSONResource(),
-        });
-    } catch (error: unknown) {
-        if (error instanceof postgres.PostgresError) {
-            throw new QueryException(error);
+        if (email === undefined) {
+            throw new ValidationException('email', ['An email is required']);
         }
 
-        throw error;
-    }
-});
+        if (password === undefined) {
+            throw new ValidationException('password', ['A password is required']);
+        }
 
-export default {
-    index,
-    login,
-    logout,
-    register,
-};
+        try {
+            const user = await User.findByEmail(email);
+
+            if ((await user?.verifyPassword(password)) === true) {
+                request.session.user = {
+                    id: user.id,
+                };
+                response.ok().jsonResponse({
+                    data: user.toJSONResource(),
+                });
+                return;
+            }
+        } catch (error) {
+            if (error instanceof ModelNotFoundException) {
+                throw new UserLoginException();
+            }
+
+            throw error;
+        }
+
+        throw new UserLoginException();
+    },
+    logout: async (request, response, next) => {
+        request.session.destroy(function (error) {
+            // request.session is now undefined here
+
+            if (error) {
+                next(error);
+            }
+
+            response.ok().jsonResponse({
+                data: null,
+                message: 'Successfully logged out.',
+            });
+        });
+    },
+    register: async (request, response) => {
+        const {birthday, email, first_name, last_name, password, password_confirm} = request.body;
+
+        if (
+            birthday === undefined ||
+            email === undefined ||
+            first_name === undefined ||
+            last_name === undefined ||
+            password === undefined
+        ) {
+            throw new ValidationException('', ['']);
+        }
+
+        if (password !== password_confirm) {
+            throw new ValidationException('password', ['Passwords do not match.']);
+        }
+
+        await passwords.validatePasswordPattern(password);
+
+        try {
+            const user = await User.create({
+                birthday: DateTime.fromISO(birthday, {setZone: false}).toJSDate(),
+                email,
+                first_name,
+                last_name,
+                password,
+            });
+
+            request.session.user = user;
+
+            response.created().jsonResponse({
+                data: user.toJSONResource(),
+            });
+        } catch (error: unknown) {
+            if (error instanceof postgres.PostgresError) {
+                throw new QueryException(error);
+            }
+
+            throw error;
+        }
+    },
+});
