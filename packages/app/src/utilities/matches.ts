0d5ecdeaf5de9073265a01861a1adae955c457ad
Rodrigo Bondoc <rbondoc96@gmail.com>
2023-09-30 19:11:30 -0700

    docs(app): add doc comment for `matches` function

--- packages/app/src/utilities/matches.ts
+++ packages/app/src/utilities/matches.ts
@@ -1,3 +1,9 @@
+/**
+ * A utility function that asserts if a value's type matches the given predicate.
+ *
+ * - If it matches, it returns the value with its narrowed, correct type.
+ * - Otherwise, it returns `false`.
+ */
 export default function matches<TValueType extends TValue, TValue = unknown>(
     value: TValue,
     predicate: (value: TValue) => value is TValueType,
