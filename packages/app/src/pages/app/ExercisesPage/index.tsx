0a70b52d790053f3fc08204c9fbef1ba4a16214e
Rodrigo Bondoc <rbondoc96@gmail.com>
2023-10-07 08:02:38 -0700

    feat(app): add muscle and muscle group sections to Exercise page

--- packages/app/src/pages/app/ExercisesPage/index.tsx
+++ packages/app/src/pages/app/ExercisesPage/index.tsx
@@ -1,11 +1,52 @@
-import {type Component} from 'solid-js';
+import {type Component, createMemo} from 'solid-js';
 
-import styles from './styles.module.scss';
+import CardScroller, {type CardProps} from '@/components/CardScroller';
+import {useMuscleGroupList, useMuscleList} from '@/stores/muscle.store';
 
 const ExercisesPage: Component = () => {
+    const muscles = useMuscleList();
+    const muscleGroups = useMuscleGroupList();
+
+    const musclesAsItems = createMemo<CardProps[]>(() => muscles()
+        .map(muscle => ({
+            href: `/app/muscles/${muscle.id}`,
+            image: muscle.image_source === null
+                ? undefined
+                : {
+                    alt: muscle.simple_name ?? muscle.name,
+                    src: muscle.image_source,
+                },
+            title: muscle.simple_name ?? muscle.name,
+        })),
+    );
+
+    const muscleGroupsAsItems = createMemo<CardProps[]>(() => muscleGroups()
+        .map(group => ({
+            href: `/app/muscle-groups/${group.id}`,
+            image: group.image_source === null
+                ? undefined
+                : {
+                    alt: group.name,
+                    src: group.image_source,
+                },
+            title: group.name,
+        })),
+    );
+
     return (
-        <section class={styles.main}>
+        <section class="flex flex-col px-4">
             <h1>Exercises Page</h1>
+
+            <div class="relative">
+                <CardScroller
+                    title="Exercises by Muscle Group"
+                    items={muscleGroupsAsItems()}
+                />
+                <CardScroller
+                    title="Exercises by Muscle"
+                    items={musclesAsItems()}
+                />
+            </div>
         </section>
     );
 };
