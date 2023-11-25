f914d2a67ce02ad79d581da5566bc9cb06756c21
Rodrigo Bondoc <rbondoc96@gmail.com>
2023-11-25 15:47:23 -0800

    build(api): add tracing deps and begin using tracing for logs

--- packages/api/core/src/main.rs
+++ packages/api/core/src/main.rs
@@ -27,16 +27,22 @@ i18n!("lang", fallback = "en");
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+    tracing_subscriber::fmt()
+        .without_time()
+        .with_target(false)
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+
     let router = crate::http::init().await?;
     let server_config = config().server();
     let server_address = SocketAddr::from(([127, 0, 0, 1], server_config.port()));
     let server = axum::Server::bind(&server_address)
         .serve(router.into_make_service());
 
-    println!("Server listening at http://{}", &server_address);
+    tracing::debug!("Server listening at http://{}", &server_address);
 
     if let Err(e) = server.await {
-        eprintln!("Server error: {}", e);
+        tracing::error!("Server error: {}", e);
         std::process::abort();
     }
 
