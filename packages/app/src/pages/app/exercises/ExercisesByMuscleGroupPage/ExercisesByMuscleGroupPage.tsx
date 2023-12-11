c8ea1f0f1bf7795d0105dc12ea1eb3edf44b7e34
Rodrigo Bondoc <rbondoc96@gmail.com>
2023-12-10 17:06:26 -0800

    feat(app): add paging to exercises by muscle group page

--- packages/app/src/pages/app/exercises/ExercisesByMuscleGroupPage/ExercisesByMuscleGroupPage.tsx
+++ packages/app/src/pages/app/exercises/ExercisesByMuscleGroupPage/ExercisesByMuscleGroupPage.tsx
@@ -1,20 +1,35 @@
-import {useParams} from '@solidjs/router';
-import {type Component, createResource, For} from 'solid-js';
+import {A as Link, useParams, useSearchParams} from '@solidjs/router';
+import {type Component, createResource, For, Show, onMount} from 'solid-js';
 
 import {ExerciseAPI} from '@/api';
-import {useUser} from '@/stores/auth.store';
 
 type PageParams = {
     id: string;
 };
 
+type SearchParams = {
+    page: string;
+    per_page: string;
+};
+
 const ExercisesByMuscleGroupPage: Component = () => {
     const params = useParams<PageParams>();
-    const user = useUser();
+    const [searchParams, setSearchParams] = useSearchParams<SearchParams>();
+
+    onMount(() => {
+        setSearchParams({
+            page: '1',
+            per_page: '10',
+        });
+    });
 
     const [exercises] = createResource(
-        () => user(),
-        async () => await ExerciseAPI.listMuscles({muscle_group: params.id}),
+        () => ({
+            muscle_group: params.id,
+            page: searchParams.page,
+            per_page: searchParams.per_page,
+        }),
+        ExerciseAPI.listMuscles,
         {
             initialValue: [],
         },
@@ -27,15 +42,46 @@ const ExercisesByMuscleGroupPage: Component = () => {
             </span>
             <For each={exercises()}>
                 {exercise => (
-                    <div class="border border-black rounded-md px-2 py-2">
-                        <div class="flex flex-col">
-                            <span>
-                                {exercise.name}
-                            </span>
+                    <Link href={`/app/exercises/${exercise.id}`}>
+                        <div class="border border-black rounded-md px-2 py-2">
+                            <div class="flex flex-col">
+                                <div class="flex flex-col">
+                                    <span>
+                                        {exercise.name}
+                                    </span>
+                                    <Show when={exercise.primary_muscles.length > 0}>
+                                        <span class="text-sm">
+                                            Primary Muscle(s): {exercise.primary_muscles.map(muscle => muscle.name).join(', ')}
+                                        </span>
+                                    </Show>
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                    </Link>
                 )}
             </For>
+            <div class="flex">
+                <Show when={Number(searchParams.page) > 1}>
+                    <button
+                        class="flex-1 border border-black rounded-md px-2 py-2"
+                        onClick={() => setSearchParams({
+                            page: (Number(searchParams.page) - 1).toString(),
+                            per_page: searchParams.per_page,
+                        })}
+                    >
+                        Previous Page
+                    </button>
+                </Show>
+                <button
+                    class="flex-1 border border-black rounded-md px-2 py-2"
+                    onClick={() => setSearchParams({
+                        page: (Number(searchParams.page) + 1).toString(),
+                        per_page: searchParams.per_page,
+                    })}
+                >
+                    Next Page
+                </button>
+            </div>
         </section>
     );
 };
