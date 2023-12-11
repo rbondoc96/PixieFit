31f2feb8e43a1c8523b12a6aec39923d68903676
Rodrigo Bondoc <rbondoc96@gmail.com>
2023-12-11 01:03:21 -0800

    update(app): restructure exercise by muscle group card UI

--- packages/app/src/pages/app/exercises/ExercisesByMuscleGroupPage/ExercisesByMuscleGroupPage.tsx
+++ packages/app/src/pages/app/exercises/ExercisesByMuscleGroupPage/ExercisesByMuscleGroupPage.tsx
@@ -40,26 +40,28 @@ const ExercisesByMuscleGroupPage: Component = () => {
             <span class="text-3xl font-extrabold tracking-tight">
                 Exercises by Muscle Group
             </span>
-            <For each={exercises()}>
-                {exercise => (
-                    <Link href={`/app/exercises/${exercise.id}`}>
-                        <div class="border border-black rounded-md px-2 py-2">
-                            <div class="flex flex-col">
+            <div class="flex flex-wrap gap-x-2 gap-y-2">
+                <For each={exercises()}>
+                    {exercise => (
+                        <Link href={`/app/exercises/${exercise.id}`}>
+                            <div class="w-36 h-36 border border-black rounded-md px-2 py-2">
                                 <div class="flex flex-col">
-                                    <span>
-                                        {exercise.name}
-                                    </span>
-                                    <Show when={exercise.primary_muscles.length > 0}>
-                                        <span class="text-sm">
-                                            Primary Muscle(s): {exercise.primary_muscles.map(muscle => muscle.name).join(', ')}
+                                    <div class="flex flex-col-reverse gap-y-2">
+                                        <span>
+                                            {exercise.name}
                                         </span>
-                                    </Show>
+                                        <Show when={exercise.primary_muscles.length > 0}>
+                                            <span class="text-sm">
+                                                {exercise.primary_muscles.map(muscle => muscle.name).join(', ')}
+                                            </span>
+                                        </Show>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </Link>
-                )}
-            </For>
+                        </Link>
+                    )}
+                </For>
+            </div>
             <div class="flex">
                 <Show when={Number(searchParams.page) > 1}>
                     <button
