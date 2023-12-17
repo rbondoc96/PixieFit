df9de14538d43ccca9bdce54fb83f35c4905e4d3
Rodrigo Bondoc <rbondoc96@gmail.com>
2023-12-16 16:01:46 -0800

    update: remove TS server code

--- packages/server/src/core/config/api.ts
+++
@@ -1,5 +0,0 @@
-import env from '@/core/env';
-
-export default {
-    apiNinjaKey: env('API_NINJA_KEY', ''),
-};
--- packages/server/src/core/config/server.ts
+++
@@ -1,6 +0,0 @@
-import env from '@/core/env';
-
-export default {
-    port: env('SERVER_PORT', 4000),
-    sessionSecret: env('SESSION_SECRET', ''),
-};
